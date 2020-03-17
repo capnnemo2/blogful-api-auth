0cb6e0578e01ba2e3fa21e1576a6c8a3eb31a83b
Ben Hernandez <benjammin234@gmail.com>
2020-03-16 19:01:57 -0700

    New comments pull user name from log in

--- test/comments-endpoints.spec.js
+++ test/comments-endpoints.spec.js
@@ -32,8 +32,7 @@ describe("Comments Endpoints", function() {
       const testUser = testUsers[0];
       const newComment = {
         text: "Test new comment",
-        article_id: testArticle.id,
-        user_id: testUser.id
+        article_id: testArticle.id
       };
       return supertest(app)
         .post("/api/comments")
@@ -61,7 +60,7 @@ describe("Comments Endpoints", function() {
             .then(row => {
               expect(row.text).to.eql(newComment.text);
               expect(row.article_id).to.eql(newComment.article_id);
-              expect(row.user_id).to.eql(newComment.user_id);
+              expect(row.user_id).to.eql(testUser.id);
               const expectedDate = new Date().toLocaleString("en", {
                 timeZone: "UTC"
               });
@@ -71,14 +70,13 @@ describe("Comments Endpoints", function() {
         );
     });
 
-    const requiredFields = ["text", "user_id", "article_id"];
+    const requiredFields = ["text", "article_id"];
 
     requiredFields.forEach(field => {
       const testArticle = testArticles[0];
       const testUser = testUsers[0];
       const newComment = {
         text: "Test new comment",
-        user_id: testUser.id,
         article_id: testArticle.id
       };
 
