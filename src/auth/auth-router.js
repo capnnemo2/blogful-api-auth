bf3131a6f9adfebc31569c2884781f2a93c7d7b7
Ben Hernandez <benjammin234@gmail.com>
2020-05-07 16:21:11 -0700

    Adds auth router and basic test

---
+++ src/auth/auth-router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const authRouter = express.Router();
+
+authRouter.post("/login", (req, res, next) => {
+  res.send("ok");
+});
+
+module.exports = authRouter;
