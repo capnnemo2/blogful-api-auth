bc88b53a01742688f0e3d9f8c57df29c65327d90
Ben Hernandez <benjammin234@gmail.com>
2020-05-08 13:59:19 -0700

    Adds refresh endpoint

--- src/config.js
+++ src/config.js
@@ -4,4 +4,5 @@ module.exports = {
   DB_URL:
     process.env.DB_URL || "postgresql://dunder_mifflin@localhost/blogful-auth",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
 };
--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -41,43 +41,64 @@ describe("Auth Endpoints", function () {
           .expect(400, { error: `Missing '${field}' in request body` });
       });
     });
+
+    it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+      const userInvalidUser = { user_name: "user-not", password: "existy" };
+      return supertest(app)
+        .post("/api/auth/login")
+        .send(userInvalidUser)
+        .expect(400, { error: `Incorrect user_name or password` });
+    });
+
+    it(`responds 400 'invalid user_name or password' when bad password`, () => {
+      const userInvalidPass = {
+        user_name: testUser.user_name,
+        password: "incorrect",
+      };
+      return supertest(app)
+        .post("/api/auth/login")
+        .send(userInvalidPass)
+        .expect(400, { error: `Incorrect user_name or password` });
+    });
+
+    it(`responds with 200 and JWT auth token using secret when valid credentials`, () => {
+      const userValidCreds = {
+        user_name: testUser.user_name,
+        password: testUser.password,
+      };
+      const expectedToken = jwt.sign(
+        { user_id: testUser.id },
+        process.env.JWT_SECRET,
+        {
+          subject: testUser.user_name,
+          expiresIn: process.env.JWT_EXPIRY,
+          algorithm: "HS256",
+        }
+      );
+      return supertest(app)
+        .post("/api/auth/login")
+        .send(userValidCreds)
+        .expect(200, { authToken: expectedToken });
+    });
   });
 
-  it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
-    const userInvalidUser = { user_name: "user-not", password: "existy" };
-    return supertest(app)
-      .post("/api/auth/login")
-      .send(userInvalidUser)
-      .expect(400, { error: `Incorrect user_name or password` });
-  });
+  describe(`POST /api/auth/refresh`, () => {
+    this.beforeEach("insert users", () => helpers.seedUsers(db, testUsers));
 
-  it(`responds 400 'invalid user_name or password' when bad password`, () => {
-    const userInvalidPass = {
-      user_name: testUser.user_name,
-      password: "incorrect",
-    };
-    return supertest(app)
-      .post("/api/auth/login")
-      .send(userInvalidPass)
-      .expect(400, { error: `Incorrect user_name or password` });
-  });
-
-  it(`responds with 200 and JWT auth token using secret when valid credentials`, () => {
-    const userValidCreds = {
-      user_name: testUser.user_name,
-      password: testUser.password,
-    };
-    const expectedToken = jwt.sign(
-      { user_id: testUser.id },
-      process.env.JWT_SECRET,
-      {
-        subject: testUser.user_name,
-        algorithm: "HS256",
-      }
-    );
-    return supertest(app)
-      .post("/api/auth/login")
-      .send(userValidCreds)
-      .expect(200, { authToken: expectedToken });
+    it(`responds 200 and JWT auth token using secret`, () => {
+      const expectedToken = jwt.sign(
+        { user_id: testUser.id },
+        process.env.JWT_SECRET,
+        {
+          subject: testUser.user_name,
+          expiresIn: process.env.JWT_EXPIRY,
+          algorithm: "HS256",
+        }
+      );
+      return supertest(app)
+        .post("/api/auth/refresh")
+        .set("Authorization", helpers.makeAuthHeader(testUser))
+        .expect(200, { authToken: expectedToken });
+    });
   });
 });
