11dc7e2e7a26ea850d93d817cb39794c8584adc4
Ben Hernandez <benjammin234@gmail.com>
2020-05-08 11:36:21 -0700

    All users endpoints tests passing

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,3 +1,6 @@
+const xss = require("xss");
+const bcrypt = require("bcryptjs");
+
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/;
 
 const UsersService = {
@@ -7,7 +10,13 @@ const UsersService = {
       .first()
       .then((user) => !!user);
   },
-
+  insertUser(db, newUser) {
+    return db
+      .insert(newUser)
+      .into("blogful_users")
+      .returning("*")
+      .then(([user]) => user);
+  },
   validatePassword(password) {
     if (password.length < 8) {
       return "Password must be longer than 8 characters";
@@ -22,6 +31,18 @@ const UsersService = {
       return "Password must contain 1 upper case, lower case, number, and special character";
     }
     return null;
+  },
+  hashPassword(password) {
+    return bcrypt.hash(password, 12);
+  },
+  serializeUser(user) {
+    return {
+      id: user.id,
+      full_name: xss(user.full_name),
+      user_name: xss(user.user_name),
+      nickname: xss(user.nickname),
+      date_created: new Date(user.date_created),
+    };
   },
 };
 
